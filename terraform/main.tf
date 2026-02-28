ae3e2b9bfd6a54e4ea9a80d4da61c27c8ad5fa19
Anusha0317 <anushareddys05170@gmail.com>
2026-02-28 04:40:52 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.region
 }
 
+# Get latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
@@ -12,43 +13,22 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "devops-sg"
-  description = "Allow SSH and HTTP"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Use existing Security Group
+data "aws_security_group" "web_sg" {
+  name = "devops-sg"
 }
 
+# Use existing Key Pair
 data "aws_key_pair" "deployer" {
   key_name = "anu-key"
 }
 
+# Create EC2 instance
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   key_name               = data.aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.web_sg.id]
 
   tags = {
     Name = "DevOps-Cloud-Automation"
