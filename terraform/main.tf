23488dff780b082b7cdeca753bb9c466dffef5ad
Anusha0317 <anushareddys05170@gmail.com>
2026-02-28 00:40:55 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,59 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = var.region
 }
 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+
+resource "aws_security_group" "web_sg" {
+  name        = "devops-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "anu-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
 resource "aws_instance" "web" {
-  ami           = "ami-0c1ac872bdc2c19e3"
-  instance_type = "t3.micro"
-  key_name      = "anu-key"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "DevOps-Cloud-Automation"
